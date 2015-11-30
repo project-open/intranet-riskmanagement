b54ee7fee80b7252c668c57239c3eacd4da52429
Frank Bergmann <frank.bergmann@project-open.com>
2015-11-30 16:49:37 +0000

    - Fixed drop script for intranet-riskmanagement

--- sql/postgresql/intranet-riskmanagement-drop.sql
+++ sql/postgresql/intranet-riskmanagement-drop.sql
@@ -24,6 +24,11 @@ drop function im_risk__new (
 	integer, varchar, integer,
 	varchar, integer, integer, integer
 );
+drop function im_risk__new (
+	integer, character varying, timestamp with time zone, 
+	integer, character varying, integer, 
+	integer, integer, integer, character varying 
+);
 drop function im_risk__delete(integer);
 
 
@@ -33,19 +38,27 @@ drop table im_risks;
 -- Delete entries from acs_objects
 delete from acs_objects where object_type = 'im_risk';
 
-
--- Completely delete the object type from the
--- object system
+-- Completely delete the object type from the object system
+delete from im_dynfield_layout where attribute_id in (
+	select attribute_id from im_dynfield_attributes where acs_attribute_id in (
+		select attribute_id from acs_attributes where object_type = 'im_risk'
+	)
+);
+delete from im_dynfield_attributes where acs_attribute_id in (
+	select attribute_id from acs_attributes where object_type = 'im_risk'
+);
+delete from acs_attributes where object_type = 'im_risk';
+delete from im_dynfield_layout_pages where object_type = 'im_risk';
+delete from acs_object_type_tables where object_type = 'im_risk';
+delete from im_rest_object_types where object_type = 'im_risk';
 SELECT acs_object_type__drop_type ('im_risk', 't');
-
 
 
 -----------------------------------------------------------
 -- Drop Categories
 --
-
 drop view im_risk_status;
-drop view im_risk_type;
+drop view im_risk_types;
 
 delete from im_categories where category_type = 'Intranet Risk Status';
 delete from im_categories where category_type = 'Intranet Risk Type';
